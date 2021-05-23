d4020278f385ae0e3948d85d932daddf85a93429
Melby01 <melby.okozi@student.moringaschool.com>
2021-05-23 21:53:35 +0300

    Update the changes done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,22 @@ import { FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GithubFormComponent } from './github-form/github-form.component';
-import { GithubDetailsComponent} from './github-details/github-details.component';
+import { GithubDetailsComponent } from './github-details/github-details.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { DateCountPipe } from './date-count.pipe'
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
     AppComponent,
     GithubFormComponent,
     GithubDetailsComponent,
-    NavbarComponent
+    NavbarComponent,
+    DateCountPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
     FormsModule
   ],
   providers: [],
