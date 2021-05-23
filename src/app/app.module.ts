61d91f17085b1d41a85bc3de8c7546686e9f7b32
Melby01 <melby.okozi@student.moringaschool.com>
2021-05-23 18:34:25 +0300

    Update the navbar files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GithubFormComponent } from './github-form/github-form.component';
 import { GithubDetailsComponent} from './github-details/github-details.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GithubFormComponent,
-    GithubDetailsComponent
+    GithubDetailsComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
