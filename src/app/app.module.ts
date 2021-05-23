ce410019600e252bfb79eb17e3fba594cfa27c07
Melby01 <melby.okozi@student.moringaschool.com>
2021-05-23 23:03:21 +0300

    Update the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,12 @@ import { FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GithubFormComponent } from './github-form/github-form.component';
-import { GithubDetailsComponent } from './github-details/github-details.component';
+import { GithubDetailsComponent } from './github-details.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { DateCountPipe } from './date-count.pipe';
 import { Repository } from './repo';
-import { GithubService } from './github-services'
-import { from } from 'rxjs';
+import { GithubService } from './github-services';
+//import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
