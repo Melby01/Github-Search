187a04a01115f73a98f45b92366422caa2a03be7
Melby01 <melby.okozi@student.moringaschool.com>
2021-05-23 17:25:56 +0300

    Include the github-form component

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { GithubFormComponent } from './github-form/github-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    GithubFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
